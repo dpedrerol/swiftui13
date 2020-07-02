7ae729874f1397a830ad6f98be116b4c6f92f1c8
Daniel Pedrerol <65600988+dpedrerol@users.noreply.github.com>
2020-07-02 17:09:54 +0200

    Cambio de color textos

---
+++ 01-first_app/Prueba/Prueba/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prueba
+//
+//  Created by Carlos Daniel Pedrerol on 02/07/2020.
+//  Copyright © 2020 Carlos Daniel Pedrerol. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .fontWeight(.bold)
+            .font(.system(size: 45 , design: .rounded))
+            .foregroundColor(.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
